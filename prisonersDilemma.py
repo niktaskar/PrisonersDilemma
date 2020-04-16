f5a18007b53a73686ce69611e4948259cfc3ae7f
Nikash Taskar <niktaskar@hotmail.com>
2020-04-15 19:39:28 -0500

    Logic fixes

--- prisonersDilemma.py
+++ prisonersDilemma.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 
 class Game:
 
@@ -17,6 +18,7 @@ class Game:
             else:
                 self.PLAYERS.append(Game.AD())
 
+
     def runGames(self):
         for i in range(len(self.PLAYERS)):
             for j in range(i+1, len(self.PLAYERS)):
@@ -26,7 +28,7 @@ class Game:
                     gameInstance = Game.GameInstance(player1, player2)
                     p1Action = gameInstance.player1.play()
                     p2Action = gameInstance.player2.play()
-                    print(player1.getClass() + ": " + str(p1Action) + "\t" + player2.getClass() + ": " + str(p2Action))
+                    # print(player1.getClass() + ": " + str(p1Action) + "\t" + player2.getClass() + ": " + str(p2Action))
                     if(p1Action == "C" and p2Action == "C"):
                         player1.PAYOUTS.append(gameInstance.OUTCOMES.get("CC")[0])
                         player2.PAYOUTS.append(gameInstance.OUTCOMES.get("CC")[1])
@@ -57,6 +59,65 @@ class Game:
                 elif isinstance(self.PLAYERS[j], Game.Grudger):
                     self.PLAYERS[j].OPPONENT_MOVES = list()
 
+
+    def replacePlayers(self, p, scoreList, scores):
+        count = int((len(scoreList)*p)/100)
+        print("Low Value: " + str(scoreList[int(count)-1]) + "\t High Value: " + str(scoreList[len(scoreList)-int(count)]))
+        players = self.PLAYERS
+        self.PLAYERS = list()
+        toAddAgain = set()
+        for key in scores:
+            for i in range(count, len(scoreList)):
+                if scores.get(key) == scoreList[i]:
+                    if i > len(scoreList) - count:
+                        print("HIGH VALUES" + str(key))
+                        if key.__contains__("Tit-4-Tat"):
+                            player = Game.T4T()
+                        elif key.__contains__("Grudger"):
+                            player = Game.Grudger()
+                        elif key.__contains__("Always Cooperate"):
+                            player = Game.AC()
+                        else:
+                            player = Game.AD()
+                        
+                        if len(self.PLAYERS) < self.NUM_PLAYERS:
+                            self.PLAYERS.append(player)
+
+                        # if isinstance(player, Game.T4T) and Game.T4T not in toAddAgain:
+                        #     toAddAgain.add(Game.T4T)
+                        # elif isinstance(player, Game.Grudger) and Game.Grudger not in toAddAgain:
+                        #     toAddAgain.add(Game.Grudger)
+                        # elif isinstance(player, Game.AC) and Game.AC not in toAddAgain:
+                        #     toAddAgain.add(Game.AC)
+                        # elif isinstance(player, Game.AD) and Game.AD not in toAddAgain:
+                        #     toAddAgain.add(Game.AD)
+                    else:
+                        print("MIDDLE VALUES")
+                        if key.__contains__("Tit-4-Tat"):
+                            player = Game.T4T()
+                        elif key.__contains__("Grudger"):
+                            player = Game.Grudger()
+                        elif key.__contains__("Always Cooperate"):
+                            player = Game.AC()
+                        else:
+                            player = Game.AD()
+                        
+                        if len(self.PLAYERS) < self.NUM_PLAYERS:
+                            self.PLAYERS.append(player)
+        
+        print(toAddAgain)
+        # for i in range(self.NUM_PLAYERS-len(self.PLAYERS)):
+        #     if i % toAddAgain.count == 0:
+        #         self.PLAYERS.append(toAddAgain[0])
+        #     elif i % len(toAddAgain) == 1:
+        #         self.PLAYERS.append(toAddAgain[1])
+        #     elif i % len(toAddAgain) == 2:
+        #         self.PLAYERS.append(toAddAgain[2])
+        #     else:
+        #         self.PLAYERS.append(toAddAgain[3])
+
+        print()
+        
     
     class GameInstance:
         ACTIONS = {"Cooperate": "C", "Defect": "D"}
@@ -70,13 +131,12 @@ class Game:
             self.player2 = p2
 
 
-    def __init__(self, n, m):
+    def __init__(self, *args):
         super().__init__()
-        self.NUM_PLAYERS = n
-        self.NUM_ROUNDS = m
-        self.generatePlayers()
-        self.runGames()
-        
+        if len(args)  != 0:
+            self.NUM_PLAYERS = args[0]
+            self.NUM_ROUNDS = args[1]
+
 
     '''
         Tit-4-Tat player
@@ -95,10 +155,11 @@ class Game:
             self.CURR_ROUND += 1
             if self.CURR_ROUND == 1 or len(self.OPPONENT_MOVES) == 0:
                 return Game.GameInstance.ACTIONS.get("Cooperate")
-            return self.OPPONENT_MOVES[self.CURR_ROUND-2]
+            return self.OPPONENT_MOVES[len(self.OPPONENT_MOVES)-1]
 
         def getClass(self):
             return "Tit-4-Tat"
+
 
     '''
         Grudger player
@@ -120,6 +181,7 @@ class Game:
         def getClass(self):
             return "Grudger"
 
+
     '''
         Always Cooperate player
         Always chooses cooperate
@@ -135,6 +197,7 @@ class Game:
 
         def getClass(self):
             return "Always Cooperate"
+
 
     '''
         Always Defect player
@@ -153,10 +216,59 @@ class Game:
             return "Always Defect"
 
 
+def createGraph(scores, i):
+    typeScoreDict = {"Tit-4-Tat": 0, "Grudger": 0, "AC": 0, "AD": 0}
+    for key in scores:
+        if key.__contains__("Tit-4-Tat"):
+            typeScoreDict["Tit-4-Tat"] += scores.get(key)
+        elif key.__contains__("Grudger"):
+            typeScoreDict["Grudger"] += scores.get(key)
+        elif key.__contains__("Cooperate"):
+            typeScoreDict["AC"] += scores.get(key)
+        else:
+            typeScoreDict["AD"] += scores.get(key)
+
+    print(str(typeScoreDict))
+
+    plt.bar(range(len(typeScoreDict)), list(typeScoreDict.values()), align='center')
+    plt.xticks(range(len(typeScoreDict)), list(typeScoreDict.keys()))
+    plt.savefig("PD_graphs/generation_" + str(i) + ".png")
+
+
 if __name__ == "__main__":
-    n = 12
-    m = 4
+    n = 4
+    m = 5
+    p = 25
+    k = 3
+    previous = Game()
     game = Game(n, m)
-    print("Game initialized with " + str(n) + " players and " + str(m) + " rounds")
-    for player in game.PLAYERS:
-        print(player.getClass() + ": " + str(sum(player.PAYOUTS)))
+    game.generatePlayers()
+
+    print("NUMBER OF PLAYERS IN ROUND 0: " + str(len(game.PLAYERS)))
+    game.runGames()
+
+    scores = {str(game.PLAYERS[i].getClass()+"-"+str(i)): sum(game.PLAYERS[i].PAYOUTS) for i in range(len(game.PLAYERS))}
+    print("SCORES: " + str(scores))
+    scoreList = [scores.get(val) for val in scores]
+    scoreList.sort()
+    print("SCORELIST: " + str(scoreList))
+
+    createGraph(scores, 0)
+    game.replacePlayers(p, scoreList, scores)
+    scores = {}
+    scoreList = []
+
+    for i in range(1, k):
+        print("NUMBER OF PLAYERS IN ROUND " + str(i) + ": " + str(len(game.PLAYERS)))
+
+        game.runGames()
+        createGraph(scores, i)
+
+        scores = {str(game.PLAYERS[i].getClass()+"-"+str(i)): sum(game.PLAYERS[i].PAYOUTS) for i in range(len(game.PLAYERS))}
+        print("SCORES: " + str(scores))
+        scoreList = [scores.get(val) for val in scores]
+        scoreList.sort()
+        print("SCORELIST: " + str(scoreList))
+        game.replacePlayers(p, scoreList, scores)
+        scores = {}
+        scoreList = []
